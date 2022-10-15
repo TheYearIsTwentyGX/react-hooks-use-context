c648a6a495b8ddbc4c3914efe197d762678fac62
Dylan Hays <twentygx@outlook.com>
2022-10-15 14:22:57 -0500

    commit tree 3f5ea211e1c57b5be5b8436e3fee29ba52c6d597

---
+++ src/context/theme.js
@@ -0,0 +1,16 @@
+import React, {useState}from "react";
+
+const ThemeContext = React.createContext();
+
+function ThemeProvider({children}) {
+    const [theme, setTheme] = useState("dark");
+      return (
+        <ThemeContext.Provider value={{theme, setTheme}}>
+          <main className={theme}>
+            {children}
+          </main>
+        </ThemeContext.Provider>
+      );
+}
+
+export {ThemeContext, ThemeProvider};
